6b660a7d0f2dc09fd19eaad2ea6460d33a95e03b
John Venpin <john.venpin@intelligensi.ai>
2025-06-01 21:15:08 +0100

    fix:cleanup gernerativeSearch remove

--- backend/functions/src/index.ts
+++ backend/functions/src/index.ts
@@ -8,9 +8,8 @@ import { updateHomepage } from "./routes/openaiRoutes";
 import { fetchusers, updateuser, fetchuser } from "./routes/userRoutes";
 import drupal7Router from "./migrations/drupal7Migrations";
 import { checkWeaviate, writeSchema, writeWeaviate } from "./routes/weaviateRoutes";
-import { generativeSearch } from "./routes/weaviateSearch";
 import { createSchema } from "./routes/schemaRoutes";
-import { deleteSite } from "./routes/siteRoutes"; // <-- Import new function
+import { deleteSite } from "./routes/siteRoutes";
 import { createDrupalSite } from "./routes/windsailRoutes";
 import { simpleSearch } from "./routes/WeaviatesSimplesearch";
 
@@ -70,7 +69,6 @@ export {
   checkWeaviate,
   writeSchema,
   writeWeaviate,
-  generativeSearch,
   createSchema,
   deleteSite,
   createDrupalSite,
